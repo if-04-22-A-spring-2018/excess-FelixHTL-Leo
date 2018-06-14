a60af10772590fe45bac0f725e02cf090380cb76
FelixHTL-Leo <31956995+FelixHTL-Leo@users.noreply.github.com>
2018-06-14 12:07:29 +0200

    Add files via upload

--- test_ioctl.c
+++ test_ioctl.c
@@ -7,8 +7,21 @@
 int
 main(int argc,char *argv[])
 {
+	char buffer[100];
+	int i = 0;
+	FILE *fd = fopen(argv[0], "r");
+	if(fd != NULL)
+	{
+		fgets(buffer, 100, fd);
+	}
+	while((i = read(0, buffer, sizeof(buffer))) != 0)
+	{
+		printf("Char: %c\n", i);
+		fflush(stdout);
+		write(1, buffer, i);
+	}
+	print(buffer);
 	struct winsize ws;
-
 	if (ioctl(0,TIOCGWINSZ,&ws)!=0) {
 		fprintf(stderr,"TIOCGWINSZ:%s\n",strerror(errno));
 		exit(-1);
@@ -16,4 +29,13 @@ main(int argc,char *argv[])
 	printf("row=%d, col=%d, xpixel=%d, ypixel=%d\n",
 	ws.ws_row,ws.ws_col,ws.ws_xpixel,ws.ws_ypixel);
 	return 0;
+}
+
+void print(char buffer[])
+{
+	for(int i = 0; i < 100; i++)
+	{
+		printf("%s ", buffer[i]);
+	}
+	printf("\n");
 }
